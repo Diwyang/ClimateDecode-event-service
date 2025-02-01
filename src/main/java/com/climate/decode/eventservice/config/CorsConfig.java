1aa58e42eeda567692d545e78af85af6a3df68a5
Diwyang Jain <Diwyang.Jain@teliolabs.com>
2025-02-01 22:39:54 +0530

    CORS Issue

---
+++ src/main/java/com/climate/decode/eventservice/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.climate.decode.eventservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins("http://20.197.13.9:4173", "http://20.197.13.9", "*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
+
